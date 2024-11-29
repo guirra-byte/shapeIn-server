433aa200f77aa47e0c92105c1b13fa54d3ea56b0
guirra-byte <guirramatheus1@gmail.com>
2024-11-29 10:22:27 -0300

    feat: implements cache with redis, pgsql db and event architecture

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,95 @@
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import { createClient, RedisClientType } from "redis";
+import { PrismaClient } from "@prisma/client";
+import { EventEmitter } from "node:events";
 import ngrok from "@ngrok/ngrok";
 import { nanoid } from "nanoid";
 import QrCode from "qrcode-terminal";
-const fastify = Fastify({ logger: true });
+import z from "zod";
 
-interface LifeshaperUrl {
-  name: string;
-  tmp_url: string;
+interface Class {
+  date: string;
+  theme: string; 
+  speaker: string;
+  stage: string;
 }
 
-const lifeshaperUrl: LifeshaperUrl[] = [];
+interface Lifeshaper {
+  id: string;
+  stage: string;
+  name: string;
+  cpf: string;
+  birth_date: string;
+  conn: RedirectUrl[];
+}
+
+interface RedirectUrl {
+  id: string;
+  tmp_url: string;
+  status: string;
+}
+
+let redis;
+const fastify = Fastify({ logger: true });
+const prismaClient = new PrismaClient();
+const handshakeRedisConnection = async () => {
+  try {
+    redis = await createClient().connect();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const lifeshapersUrl: RedirectUrl[] = [];
+interface IRepository<T> {
+  save(data: T): Promise<void>;
+  getById(id: string): Promise<T | undefined>;
+}
+
+class LifeshaperRepository implements IRepository<Lifeshaper> {
+  constructor(private redisClient: RedisClientType) {}
+  async save(data: Lifeshaper): Promise<void> {
+    await this.redisClient.set(`${data.id}`, JSON.stringify(data));
+  }
+
+  async getById(id: string): Promise<Lifeshaper | undefined> {
+    const lifeshaper = await this.redisClient.get(id);
+    if (lifeshaper) return JSON.parse(lifeshaper) as Lifeshaper;
+    return undefined;
+  }
+}
+
+class ClassRepository implements IRepository<Class> {
+  constructor(private redisClient: RedisClientType) {}
+}
+
+class CheckIn extends EventEmitter {
+  private static _instance: CheckIn;
+  private constructor() {
+    super();
+  }
+
+  public static get instance() {
+    if (!this._instance) {
+      this._instance = new CheckIn();
+    }
+
+    return this._instance;
+  }
+}
+
+const checkIn = CheckIn.instance;
+const checkInConsumer = () => {
+  checkIn.on("did", (id: string) => {
+    const currentLifeshaper = lifeshapersUrl.find((url) => url.id === id);
+    if (!currentLifeshaper) {
+      throw new Error("Resource not found!");
+    }
+  });
+
+  checkIn.on("register", (data: Lifeshaper) => {});
+};
+
 fastify.route({
   method: "GET",
   url: "/checkin/:id",
@@ -17,22 +97,43 @@ fastify.route({
     params: {
       type: "object",
       properties: {
-        id: { type: "string" },
+        id: { type: "string" }
       },
-      required: ["id"],
-    },
+      required: ["id"]
+    }
   },
   handler: async (request: FastifyRequest, reply: FastifyReply) => {
     const { id } = request.params as { id: string };
+    checkIn.emit("did", id);
     reply.send("<h1>Registrado</h1>");
-  },
+  }
+});
+
+const registerSchema = z.object({
+  class: z.string(),
+  name: z.string(),
+  cpf: z.string(),
+  birth_date: z.string()
+});
+
+fastify.route({
+  method: "POST",
+  url: "/checkin/register",
+  handler: async (request: FastifyRequest, reply: FastifyReply) => {
+    const data = registerSchema.parse(request.body);
+  }
 });
 
 async function mountRedirectUrl(data: any, appUrl: string) {
   const currentDataId = nanoid();
   const uniqueUrl = `${appUrl}/checkin/${currentDataId}`;
-  lifeshaperUrl.push({ name: data.name, tmp_url: uniqueUrl });
+  const transferData = {
+    name: data.name,
+    tmp_url: uniqueUrl,
+    id: currentDataId
+  };
 
+  checkIn.emit("register", transferData);
   return uniqueUrl;
 }
 
@@ -44,10 +145,10 @@ async function main() {
       const data = JSON.stringify({
         id: 6,
         name: "Micaele Castro",
-        class: "Shapers",
+        stage: "Shapers",
         class_date: "2024-11-28T10:00:00",
         status: "PRESENTE",
-        arrived_at: null,
+        arrived_at: null
       });
 
       await ngrok
@@ -68,3 +169,5 @@ async function main() {
 }
 
 main();
+checkInConsumer();
+handshakeRedisConnection();
