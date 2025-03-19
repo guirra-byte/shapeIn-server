b10fab0b907cbb76c7faa399da8b802636632318
guirra-byte <guirramatheus1@gmail.com>
2025-03-19 08:36:39 -0300

    update: create handlers for each of application services

--- src/server.ts
+++ src/server.ts
@@ -1,169 +1,60 @@
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import ngrok from "@ngrok/ngrok";
-import { nanoid } from "nanoid";
-import QrCode from "qrcode";
 import fs from "node:fs";
-import z from "zod";
-import { Resend } from "resend";
 import fastifyCors from "@fastify/cors";
-import path from "node:path";
+import fastifyQuerystring from "fastify-qs";
 import dotenv from "dotenv";
+import { AddUserBatchHandler } from "./handlers/add-user.handler";
+import { UserRepository } from "./repository/user/user-repository";
+import {
+  SseHandshakeHandler,
+  SseNotifyHandler,
+} from "./handlers/sse-client.handler";
+import { CheckinHandler } from "./handlers/checkin.handler";
+import { LoginHandler } from "./handlers/auth/login-handlers";
+import { RefreshTokenHandler } from "./handlers/auth/refresh-token.handler";
 
-interface CheckIn {
-  id: string;
-  arrived_at: string;
-  email: string;
-  name: string;
-  status: string;
-  stage: string;
-}
-
-const toDoCheckin: CheckIn[] = [];
-const qrcodesFilepaths: { id: string; filepath: string }[] = [];
-const checkinDb: CheckIn[] = [];
-const sseClients: FastifyReply[] = [];
-
-let APP_URL = "";
 dotenv.config();
+
 const fastify = Fastify({ logger: true });
+fastify.register(fastifyQuerystring, {});
 fastify.register(fastifyCors, {
   origin: "*",
+  credentials: true,
 });
 
-const corsHeaders: { key: string; value: string }[] = [
-  { key: "Access-Control-Allow-Origin", value: "*" },
-  { key: "Access-Control-Allow-Methods", value: "*" },
-];
-const sseHeaders: { key: string; value: string }[] = [
-  { key: "Content-Type", value: "text/event-stream" },
-  { key: "Cache-Control", value: "no-cache" },
-  { key: "Connection", value: "keep-alive" },
-];
-
-async function mountRedirectUrl(id: string, appUrl: string) {
-  const uniqueUrl = `${appUrl}/checkin/${id}`;
-  return uniqueUrl;
-}
-
-const resend = new Resend(process.env.RESEND_KEY);
-const fetchQrCode = async (qrcodeUrl: string, to: string[]) => {
-  const { error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to,
-    subject: "Your Checkin Qr Code",
-    html: `<!DOCTYPE html>
-      <html>
-      <head>
-        <title>QR Code</title>
-      </head>
-      <body style="font-family: Arial, sans-serif; text-align: center; background-color: #f9f9f9; padding: 20px;">
-        <h1>Seu QR Code para realizar Checkin</h1>
-        <p>Use o QR Code abaixo para acessar:</p>
-        <img src=${qrcodeUrl} alt="QR Code" style="width: 200px; height: 200px;" />
-      </body>
-      </html>`,
-  });
-
-  if (error) {
-    console.error({ error });
-  }
-};
-
-const genQrCode = async (
-  data: { email: string; id: string },
-  redirectUrl: string
-) => {
-  const outDirPath = path.resolve(__dirname, "../files");
-  const filepath = outDirPath.concat(`/${data.id}.png`);
-  const stream = fs.createWriteStream(filepath);
-
-  await QrCode.toFileStream(stream, redirectUrl);
-  const serveQrcodeUrl = APP_URL.concat(`/checkin/qrcode/${data.id}`);
-  qrcodesFilepaths.push({ id: data.id, filepath });
-
-  await fetchQrCode(serveQrcodeUrl, [data.email]);
-};
-
-fastify.route({
-  url: "/checkin/qrcode/:id",
-  method: "GET",
-  schema: {
-    querystring: {
-      type: "object",
-      properties: { id: { type: "string" } },
-    },
-  },
-  handler: async (request: FastifyRequest, reply: FastifyReply) => {
-    const { id } = request.params as { id: string };
-    reply.header("Content-Type", "image/png");
-    const qrcodeToServe = qrcodesFilepaths.find((qrPath) => qrPath.id === id);
-    if (!qrcodeToServe) {
-      throw new Error("QrCode not found!");
-    }
-
-    return reply.send(fs.createReadStream(qrcodeToServe.filepath));
-  },
-});
-
-const addUserSchema = z.array(
-  z.object({
-    name: z.string(),
-    email: z.string().email(),
-    stage: z.string(),
-  })
+const userRepository = UserRepository.instance;
+const sseNotifyHandler = new SseNotifyHandler();
+const addUserHandler = new AddUserBatchHandler(
+  userRepository,
+  sseNotifyHandler
 );
 
 fastify.route({
   url: "/checkin/add-user",
   method: "POST",
-  handler: async (request: FastifyRequest, reply: FastifyReply) => {
-    const parseData = addUserSchema.parse(request.body);
-    for (const checkin of parseData) {
-      const usedData = checkinDb.find((checkin) => checkin.id === checkin.id);
-      if (!usedData) {
-        const checkinId = nanoid();
-        toDoCheckin.push({
-          ...checkin,
-          status: "AUSENTE",
-          arrived_at: "",
-          id: checkinId,
-        });
-
-        mountRedirectUrl(checkinId, APP_URL).then((redirectUrl) => {
-          genQrCode({ email: checkin.email, id: checkinId }, redirectUrl);
-        });
-      }
-    }
-  },
+  handler: async (request: FastifyRequest, reply: FastifyReply) =>
+    addUserHandler.handle(request, reply),
 });
 
+const sseHandshakeHandler = new SseHandshakeHandler();
 fastify.route({
-  url: "/checkin/events",
-  method: "GET",
-  handler: async (request: FastifyRequest, reply: FastifyReply) => {
-    const appHeaders = [...corsHeaders, ...sseHeaders];
-    for (const { key, value } of appHeaders) {
-      reply.raw.setHeader(key, value);
-    }
-
-    sseClients.push(reply);
-    request.raw.on("close", () => {
-      const index = sseClients.indexOf(reply);
-      if (index !== -1) {
-        sseClients.splice(index, 1);
-      }
-    });
-
-    reply.raw.write("data: Connection has been stablished!\n\n");
+  url: "/checkin/events/:type",
+  schema: {
+    params: {
+      type: "object",
+      properties: {
+        type: { type: "string" },
+      },
+      required: ["type"],
+    },
   },
+  method: "GET",
+  handler: async (request: FastifyRequest, reply: FastifyReply) =>
+    sseHandshakeHandler.execute(request, reply),
 });
 
-const notifyClients = async (data: CheckIn[]) => {
-  sseClients.forEach((_checkinClient) => {
-    _checkinClient.raw.write(`data: ${JSON.stringify(data)}\n\n`);
-  });
-};
-
+const checkinHandler = new CheckinHandler(userRepository, sseNotifyHandler);
 fastify.route({
   url: "/checkin/:id",
   schema: {
@@ -176,22 +67,35 @@ fastify.route({
     },
   },
   method: "GET",
+  handler: async (request: FastifyRequest, reply: FastifyReply) =>
+    checkinHandler.handle(request, reply),
+});
+
+// Login Handler
+const loginHandler = new LoginHandler(userRepository);
+fastify.route({
+  url: "/auth/login",
+  method: "POST",
+  handler: (request: FastifyRequest, reply: FastifyReply) =>
+    loginHandler.handle(request, reply),
+});
+
+// Renew access_token
+const refreshTokenHandler = new RefreshTokenHandler();
+fastify.route({
+  url: "/auth/refresh",
+  method: "GET",
+  handler: async (request: FastifyRequest, reply: FastifyReply) =>
+    refreshTokenHandler.handle(request, reply),
+});
+
+// Logout Handler
+fastify.route({
+  url: "/auth/logout",
+  method: "POST",
   handler: async (request: FastifyRequest, reply: FastifyReply) => {
-    const { id } = request.params as { id: string };
-    const index = toDoCheckin.findIndex((checkin) => checkin.id === id);
-
-    if (index !== -1) {
-      const currentData = toDoCheckin[index];
-      if (currentData.status === "AUSENTE") {
-        currentData.status = "PRESENTE";
-        currentData.arrived_at = new Date().toISOString();
-        checkinDb.push({
-          ...currentData,
-        });
-      }
-    }
-
-    await notifyClients(checkinDb);
+    reply.raw.removeHeader("Set-Cookie");
+    reply.status(200).send({ message: "Logout" });
   },
 });
 
@@ -207,10 +111,6 @@ async function main() {
         })
         .then(async (ngrokProxy) => {
           const proxyUrl = ngrokProxy.url();
-          if (proxyUrl !== null) {
-            APP_URL = proxyUrl;
-          }
-
           console.log(
             `Server is running on port ${connectionString} - ${proxyUrl}`
           );
